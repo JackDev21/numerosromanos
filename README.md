40071753e5db6a04a65d9cd358c942441ad3d848
JackDev21 <jose.a.c.lopez@gmail.com>
2023-10-11 22:40:51 +0200

    Cambios: conversion

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 # numerosromanos
 Jugando con números Romanos
-
 Convertir numeros romanos a numeros. Entre 0 y 3999
+
+I - Uno
+V - Cinco
+X - Diez
+C - Cien
+D - Quinientos
+M - Mil
+
+1123 ==> MCXXIII
+XXXX ________III ----- 3 * 1 = 3 == III
+XXX________XX--------- 2 * 10 = 20 == XX
+XX________C----------- 10 * 10 = 100 == C
+X_______M------------- 10 * 100 = 1000 == M
+
+
+Descomponer el número en millares, centenas, decenas, unidades.
+1
+1
+2
+3
+
+
+## Como se convierte un número entero natural menor que 400 a Romano
+
+1123 --> MCXXIII
--- romanos.py
+++ romanos.py
@@ -13,14 +13,25 @@ El numero 0 devuelve una cadena vacía
 """
 
 def convertir_romano(numero):
-
     if not isinstance (numero, str):
+        return "Error: no has escrito un numero entero"
+    
+    try:
+        numero = int (numero)
+    except ValueError:
         return "Error: no has escrito un numero entero"
 
     if numero < 1 or numero > 3999:
         return "Error: el numero debe estar entre 1 y 3999"
     
-    return "Todo: Convertir"
+    unidades = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+    decenas = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+    centenas = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+    millares = ["", "M", "MM", "MMM"]
+
+    resultado = millares[numero // 1000] + centenas[(numero % 1000) // 100] + decenas[(numero % 100) // 10] + unidades[numero % 10]
+
+    return resultado
 
 print (convertir_romano("56t"))
 print (convertir_romano("56"))
